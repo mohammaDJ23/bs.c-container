bac94aa68e7e4706a3f62cb4766928affdf7ddba
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-08-24 00:46:35 +0330

    spliting out the publicPath of the dev and prod modes and using a server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,3 +1,4 @@
+const { ModuleFederationPlugin } = require('webpack').container;
 const { merge } = require('webpack-merge');
 const path = require('path');
 const webpack = require('webpack');
@@ -13,6 +14,15 @@ module.exports = merge(commonConfig, {
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(dotenv.parsed),
     }),
+    new ModuleFederationPlugin({
+      remotes: {
+        auth: `auth@${process.env.AUTH_APP}/remoteEntry.js`,
+        bank: `bank@${process.env.BANK_APP}/remoteEntry.js`,
+      },
+    }),
   ],
   devServer: { port: 3004, historyApiFallback: true },
+  output: {
+    publicPath: `${process.env.CONTAINER_APP}/`,
+  },
 });
