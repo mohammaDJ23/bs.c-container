3493ef22190ca24e48835897475701816a8b8a84
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-09-28 17:49:41 +0330

    considering the file path of the sw

--- src/serviceWorkerRegisteration.js
+++ src/serviceWorkerRegisteration.js
@@ -1,8 +1,16 @@
+import { isProduction } from './lib';
+
 export function register() {
   if (navigator.serviceWorker) {
     window.addEventListener('load', () => {
+      let fileUrl = '/sw.bundle.js';
+
+      if (isProduction()) {
+        fileUrl = '/container/static' + fileUrl;
+      }
+
       navigator.serviceWorker
-        .register('/sw.bundle.js')
+        .register(fileUrl)
         .then((registration) => console.log('service worker registered: ', registration))
         .catch((err) => console.error('service worker registration failed: ', err));
     });
