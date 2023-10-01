3fd1afc4e6788e9b15f1b5b8e4576afc5595470a
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-10-01 05:18:12 +0330

    removing the loading of the window event

--- src/scripts/serviceWorker/serviceWorkerRegisteration.ts
+++ src/scripts/serviceWorker/serviceWorkerRegisteration.ts
@@ -1,10 +1,8 @@
 export function register() {
   if (navigator.serviceWorker) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register(process.env.CONTAINER_PUBLIC_PATH + 'sw.bundle.js', { scope: '/' })
-        .then((registration) => console.log('service worker registered: ', registration))
-        .catch((err) => console.error('service worker registration failed: ', err));
-    });
+    navigator.serviceWorker
+      .register(process.env.CONTAINER_PUBLIC_PATH + 'sw.bundle.js', { scope: '/' })
+      .then((registration) => console.log('service worker registered: ', registration))
+      .catch((err) => console.error('service worker registration failed: ', err));
   }
 }
