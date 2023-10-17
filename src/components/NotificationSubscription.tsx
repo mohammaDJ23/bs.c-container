7d95acfd6ff22467e79442ff8239d002fbfe32a1
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-10-17 16:31:44 +0330

    adding the fingerprint package and revising the notificaiton subscription

--- src/components/NotificationSubscription.tsx
+++ src/components/NotificationSubscription.tsx
@@ -1,17 +1,25 @@
 import { FC, PropsWithChildren, Fragment, useEffect } from 'react';
 import { Notifications, isUserAuthenticated } from '../lib';
+import { Fingerprint } from '../lib/fingerprint';
 
 const NotificationSubscription: FC<PropsWithChildren> = ({ children }) => {
   const isUserLoggedIn = isUserAuthenticated();
 
   useEffect(() => {
-    if (isUserLoggedIn) {
-      Notifications.subscribe().then((subscription) => {
-        if (subscription) {
-          console.log(subscription);
+    (async function () {
+      try {
+        if (isUserLoggedIn) {
+          const pushSubscription = await Notifications.subscribe();
+          if (pushSubscription) {
+            const visitorId = await Fingerprint.getVisitorId();
+            const subscription = Object.assign(pushSubscription, { visitorId });
+            console.log(subscription);
+          }
         }
-      });
-    }
+      } catch (error) {
+        console.error('Faild to subscribe the notification.', error);
+      }
+    })();
   }, [isUserLoggedIn]);
 
   return <Fragment>{children}</Fragment>;
