1116935a070c8e51d9302e10d06b63c75c5bf80a
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2023-10-25 07:18:29 +0330

    connecting the socket

--- src/components/UserServiceSocketProvider.tsx
+++ src/components/UserServiceSocketProvider.tsx
@@ -4,17 +4,6 @@ import { getUserServiceSocket } from '../lib';
 const UserServiceSocketProvider: FC<PropsWithChildren> = ({ children }) => {
   useEffect(() => {
     const socket = getUserServiceSocket();
-    socket.on('connect_error', (err) => {});
-    socket.on('users_status', (data) => {
-      const event = new CustomEvent('on-users-status', {
-        cancelable: true,
-        detail: data,
-      });
-      window.dispatchEvent(event);
-    });
-    return () => {
-      socket.removeAllListeners();
-    };
   }, []);
 
   useEffect(() => {
